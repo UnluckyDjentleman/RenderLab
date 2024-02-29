8d591834b81b7fce3191f80b762e44d13de2a14f
UnluckyDjentleman <94239070+UnluckyDjentleman@users.noreply.github.com>
2024-02-29 08:37:15 +0300

    Update 15-01.js

--- 15-01.js
+++ 15-01.js
@@ -35,17 +35,14 @@ app.get('/update/:id', (request, response)=>{
 
 app.post('/add', (request, response)=>{
     addContactMethod(request.body);
-    response.redirect('/');
 })
 
 app.post('/update/:id', (request, response)=>{
     updateContactMethod(request);
-    response.redirect('/');
 })
 
 app.post('/delete/:id', (request, response)=>{
     deleteContactMethod(request);
-    response.redirect('/');
 })
 
 app.listen(port, () => {
